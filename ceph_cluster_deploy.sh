13efce6a827cd782247a35bf454e3771526e1272
zliu <zliu@orca-1.zliu-qv32015.rdma-pg0.wisc.cloudlab.us>
2018-01-15 18:29:45 -0600

    new ceph scripts

---
+++ ceph_cluster_deploy.sh
@@ -0,0 +1,8 @@
+ceph-deploy new orca-{1,2,3}
+echo "public network = 192.168.0.0/24" >>ceph.conf
+ceph-deploy install --release luminous orca-{1,2,3}
+ceph-deploy mon create-initial
+ceph-deploy admin orga-{1,2,3}
+ceph-deploy mgr create orca-1
+ceph-deploy osd create --filestore orca-2:sdb orca-2:sdc orca-3:sdb orca-3:sdc
+
---
+++ ceph_cluster_purge.sh
@@ -0,0 +1,4 @@
+ceph-deploy purge orca-{1,2,3}
+ceph-deploy purgedata orca-{1,2,3}
+ceph-deploy forgetkeys
+rm ceph.*
